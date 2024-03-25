c2b10cb1e4f45e7138b257c11b3688fb8456e5e3
AlexWalker222 <alexw3071@gmail.com>
2024-03-25 11:15:32 -0400

    ""

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+// @ts-nocheck
 import { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   const [activeKey, setActiveKey] = useState("");
   useEffect(() => {
-    document.addEventListener('keydown', (event) => {
-      playSound(event.key.toUpperCase())
+    document.addEventListener("keydown", (event) => {
+      playSound(event.key.toUpperCase());
     });
   }, []);
 
@@ -13,82 +15,74 @@ function App() {
     {
       keyCode: 81,
       text: "Q",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
     },
     {
       keyCode: 87,
       text: "W",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
     },
     {
       keyCode: 69,
       text: "E",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
     },
     {
       keyCode: 65,
       text: "A",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
     },
     {
       keyCode: 83,
       text: "S",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
     },
     {
       keyCode: 68,
       text: "D",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
     },
     {
       keyCode: 90,
       text: "Z",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
     },
     {
       keyCode: 88,
       text: "X",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
     },
     {
       keyCode: 67,
       text: "C",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-    }
-  ];    
-function playSound(selector) {
- const audio = document.getElementById(selector);
- console.log("audio");
- audio.play();
-  setActiveKey(selector)
-}
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    console.log("audio");
+    audio.play();
+    setActiveKey(selector);
+  }
 
   return (
     <div className="App" id="drum-machine">
-        <div id="display">{activeKey}</div>
-          <div className="drum-pads">
-            {drumPads.map((drumPad) => (
-              <div
-               key={drumPad.src} 
-               onClick={() => (
-                playSound(drumPad.text)
-               )} 
-               className="drum-pad"
-               id={drumPad.src}>
-                {drumPad.text}
-                <audio 
-                  src={drumPad.src} 
-                  id={drumPad.text} 
-                  className="clip"
-                  ></audio>
-              </div>
-            ))}
+      <div id="display">{activeKey}</div>
+      <div className="drum-pads">
+        {drumPads.map((drumPad) => (
+          <div
+            key={drumPad.src}
+            onClick={() => playSound(drumPad.text)}
+            className="drum-pad"
+            id={drumPad.src}
+          >
+            {drumPad.text}
+            <audio src={drumPad.src} id={drumPad.text} className="clip" />
           </div>
-    </div> 
-);
+        ))}
+      </div>
+    </div>
+  );
 }
-
-
-
 
 export default App;
